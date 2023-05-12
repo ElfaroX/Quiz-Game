e27e688789907f7f2bd3e81238a30c09221b0652
HarounQerdachi <haroun.qerdachi12@gmail.com>
2023-05-12 12:50:37 +0200

    Some modifications is  added

--- game.py
+++ game.py
@@ -14,7 +14,7 @@ padding = (term_width - len(welcome_msg)) // 2
 print(' ' * padding + welcome_msg) # print the message with padding on both sides
 
 play = input('Do you want to play? (yes/no): ')
-if play != 'yes':
+if play.lower() != 'yes':
     quit()
 
 os.system('cls')
@@ -25,15 +25,26 @@ print(f'Hello {user_name}, please answer the following questions.')
 
 time.sleep(1)
 
-print(Questions[0])
+# print(f'{Questions[0]}?')
 
-def test(answer):
-    if answer == 16:
-        print('Your answer is correct')
-    else:
-        print('Try again')
+# def test(answer):
+#     if answer == 16:
+#         print('Your answer is correct')
+#     else:
+#         print('Try again')
 
-answer_str = input('Your answer: ')
-answer = int(answer_str)  # convert the string to an integer
-test(answer)
+# answer_str = input('Your answer: ')
+# answer = int(answer_str)  # convert the string to an integer
+# test(answer)
 
+answer = input(f'{Questions[0]}? ')
+if int(answer) == 16 :
+    print('Correct!')
+else:
+    print('Try again')
+
+answer = input(f'{Questions[1]}? ')
+if int(answer) == 56 :
+    print('Correct!')
+else:
+    print('Try again')
